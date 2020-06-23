2682b570818a714e8568b4b142b389e70d8fa987
许海涛 <xuhaitao@autohome.com.cn>
2020-06-23 16:48:42 +0800

    原生flutter混合页面

--- app/src/main/java/com/xht/flutter/NativeAndFlutterActivity.java
+++ app/src/main/java/com/xht/flutter/NativeAndFlutterActivity.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.idlefish.flutterboost.containers.FlutterFragment;
+
 /**
  * Created by xht on 2020/6/23
+ * 安卓和原生混合页面
  */
 public class NativeAndFlutterActivity extends AppCompatActivity {
 
@@ -13,5 +16,14 @@ public class NativeAndFlutterActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_native_flutter);
+
+        init();
+    }
+
+    public void init() {
+        //将Flutter页面作为Fragment添加到FrameLayout上
+        FlutterFragment flutterFragment = new FlutterFragment.NewEngineFragmentBuilder().url("fragmentPage").build();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_stub, flutterFragment).commit();
     }
 }
